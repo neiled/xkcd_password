8e0c679b70f6249f0095cd5cf3c8d5dc6422bb94
Neil Edwards <neil@plasticwater.com>
2014-04-25 22:51:24 -0400

    added generate test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -7,3 +7,12 @@ describe "Application 'xkcd_password'" do
     @app.windows.size.should == 1
   end
 end
+
+describe "password controller" do
+  tests PasswordController
+
+  it 'changes the password when generate is pressed' do
+    tap 'Generate'
+    views(UILabel).first.text.should.not.equal "Password goes here..."
+  end
+end
